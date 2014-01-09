2e149917d573d3c4a35274c22dd7f2a35e5163ed
R. M. Cuenen <Raymond.Cuenen@gmail.com>
2014-01-09 19:55:33 +0100

    Fix for "history breaks" on 'document.open()' for firefox. Opera still has this problem (maybe fixed in v13?)

--- demo/demo.js
+++ demo/demo.js
@@ -51,6 +51,7 @@ function postData() {
     } else {
         loadPlotData();
     }
+    return false;
 }
 
 /**
@@ -77,7 +78,7 @@ function loadData(data) {
     var target = document.getElementById("plotwindow");
     target = (target.contentWindow) ? target.contentWindow :
             (target.contentDocument.document) ? target.contentDocument.document : target.contentDocument;
-    target.document.open();
+    target.document.open("text/html", "replace");
     target.document.write(data);
     target.document.close();
     target.document.body.style.cssText = "margin: 0px;";
